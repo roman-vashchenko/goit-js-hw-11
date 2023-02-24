db9256f1ad07907d016f57d1d43a3a041d22f6ba
Roman Vashchenko <roman1991dp@gmail.com>
2023-02-24 14:12:59 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import PixabayApiService from './js/pixabay-servise.js';
 
 const refs = {
@@ -12,18 +13,31 @@ const pixabayApiService = new PixabayApiService();
 refs.searchForm.addEventListener('submit', onSearch);
 refs.loadMoreBtn.addEventListener('click', onLoadMore);
 
-function onSearch(event) {
-  event.preventDefault();
-  pixabayApiService.searchQuery =
-    event.currentTarget.elements.searchQuery.value.trim();
-  if (pixabayApiService.searchQuery === '') {
-    return;
-  }
-  refs.searchForm.reset();
-  pixabayApiService.resetPage();
-  pixabayApiService.fetchImages();
+async function onSearch(event) {
+  try {
+    event.preventDefault();
+    pixabayApiService.searchQuery =
+      event.currentTarget.elements.searchQuery.value.trim();
+    if (pixabayApiService.searchQuery === '') {
+      return;
+    }
+    refs.searchForm.reset();
+    pixabayApiService.resetPage();
+    const getImages = await pixabayApiService.fetchImages();
+
+    console.log(getImages);
+    if (getImages.totalHits === 0) {
+      refs.loadMoreBtn.style.display = 'none';
+      return Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    } else if (getImages.totalHits) {
+      refs.loadMoreBtn.style.display = 'block';
+      return Notify.success(`Hooray! We found ${getImages.totalHits} images.`);
+    }
+  } catch {}
 }
 
-function onLoadMore() {
-  pixabayApiService.fetchImages();
+async function onLoadMore() {
+  const loadMoreImages = await pixabayApiService.fetchImages();
 }
--- src/js/pixabay-servise.js
+++ src/js/pixabay-servise.js
@@ -1,18 +1,22 @@
+const axios = require('axios').default;
+
 export default class PixabayApiService {
   constructor() {
     this.searchQuery = '';
     this.page = 1;
   }
 
-  fetchImages() {
+  async fetchImages() {
     const url = `https://pixabay.com/api/?key=33776129-06b0afe52f3ec0e98d1b43427&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`;
 
-    return fetch(url)
-      .then(response => response.json())
-      .then(({ hits }) => {
-        console.log(hits);
-        this.page += 1;
-      });
+    const response = await axios.get(url);
+
+    // return fetch(url)
+    //   .then(response => response.json())
+    //   .then(({ hits }) => {
+    //     console.log(hits);
+    this.page += 1;
+    return response.data;
   }
   resetPage() {
     this.page = 1;
